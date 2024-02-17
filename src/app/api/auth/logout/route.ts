08d2571edb65f7798ff7fe6fa8c703b65c4179ed
Loki <saintlukke@gmail.com>
2024-02-17 12:31:51 +0300

    change

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -4,14 +4,19 @@ import { cookies } from "next/headers";
 export async function POST(req: Request) {
     const token = cookies().get('token')
   const data = await req.json()
-  let response = await autoAPI.post(`/auth/logout`, JSON.stringify(data), {
-    headers: {
-        'Authorization': 'Bearer '+ token?.value
-    },
-    validateStatus: function (status) {
-      return status < 500; // Resolve only if the status code is less than 500
-    }
-  })
+  let response = null
+  try {
+    response = await autoAPI.post(`/auth/logout`, JSON.stringify(data), {
+      headers: {
+          'Authorization': 'Bearer '+ token?.value
+      },
+      validateStatus: function (status) {
+        return status < 500; // Resolve only if the status code is less than 500
+      }
+    })
+  } catch (error) {
+    console.log(error)
+  }
   if (response) {    
     if (response.status == 200) {
         cookies().delete('user')
