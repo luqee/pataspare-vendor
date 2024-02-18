d956c30af0d59ddcf69bc3c8d1cf0405aa14ebbd
Loki <saintlukke@gmail.com>
2024-02-18 23:53:03 +0300

    fix private route error

--- src/components/PrivateComponent.jsx
+++ src/components/PrivateComponent.jsx
@@ -1,9 +1,15 @@
 'use client'
 import { useAuthContext } from '@/context/AuthContext';
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react';
 
 export const RequireAuth = ({ children})=> {
     let {user} = useAuthContext()
     const router = useRouter()
-    return user ? children: router.push('/auth/login')
+    useEffect(()=>{
+        if (!user) {
+            router.push('auth/login')
+        }
+    }, [])
+    return children
 }
